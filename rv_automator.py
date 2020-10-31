b122f97fac4fd115fc176325f328b8628cea9b5c
Jarrett Er <happymanjarrett@outlook.com>
2020-10-31 14:25:55 +0800

    2 new events
    and some other things

--- rv_automator.py
+++ rv_automator.py
@@ -1,32 +1,129 @@
 import random
+import copy
+from time import sleep
+def sorter(the_dict):
+    the_dict = sorted(the_dict.items(), key=lambda x: x[1], reverse=True)
+    the_dict = dict(the_dict)
+
+def eliminator(points_dict, participants_list):
+    if elimination == 0:
+        participants_list.sort()
+        print(points_dict)
+        print('Did not eliminate any contestant.')
+        print()
+        sleep(1)
+    elif elimination == 1:
+        participants_list.sort()
+        eliminated_first = list(points_dict)[len(points_dict) - 1]
+        print(points_dict)
+        print()
+        points_dict.popitem()
+        participants_list.pop()
+        sleep(1)
+        print(f'Eliminated {elimination} contestant who is {eliminated_first}.')
+        print()
+        print(points_dict)
+        sleep(1)
+    elif elimination == 2:
+        participants_list.sort()
+        eliminated_first = list(points_dict)[len(points_dict) - 1]
+        eliminated_second = list(points_dict)[len(points_dict) - 2]
+        print(points_dict)
+        print()
+        points_dict.popitem()
+        points_dict.popitem()
+        participants_list.pop()
+        participants_list.pop()
+        sleep(1)
+        print(f'Eliminated {elimination} contestants who are {eliminated_second} and {eliminated_first}.')
+        print()
+        print(points_dict)
+        sleep(1)
+    elif elimination == 3:
+        participants_list.sort()
+        eliminated_first = list(points_dict)[len(points_dict) - 1]
+        eliminated_second = list(points_dict)[len(points_dict) - 2]
+        eliminated_third = list(points_dict)[len(points_dict) - 3]
+        print(points_dict)
+        print()
+        points_dict.popitem()
+        points_dict.popitem()
+        points_dict.popitem()
+        participants_list.pop()
+        participants_list.pop()
+        participants_list.pop()
+        sleep(1)
+        print(f'Eliminated {elimination} contestants who are {eliminated_third}, {eliminated_second} and {eliminated_first}.')
+        print()
+        print(points_dict)
+        sleep(1)
 
 # event functions
+
+# wheel of fortune type
 def wheel_of_fortune():
-    contestants_list_1 = contestants_list
-    points_1 = dict()
-    number_of_contestants_1 = len(contestants_list_1)
-    contestants_eliminated_1 = 1
-    starting_number_1 = 100 * number_of_contestants_1
-    x_1 = 1
-    contestant_points_1 = dict()
-    while x_1 <= number_of_contestants_1:
+    contestant_points = dict()
+    contestant_points = copy.deepcopy(contestant_points)
+    print()
+    print('Wheel of Fortune')
+    print('Try to be selected first')
+    points = dict()
+    number_of_contestants = len(main_contestants_list)
+    contestants_selected = 1
+    starting_number = 1000
+    x = 1
+    
+    while x <= number_of_contestants:
         # append points to dictionary
-        points_1.update({x_1: starting_number_1})
+        points.update({x: starting_number})
         # increase x by 1
-        x_1 += 1
-        starting_number_1 -= 21
-        print(points_1)
-    while contestants_eliminated_1 <= number_of_contestants_1:
-        eliminated_one_1 = random.choice(contestants_list_1)
-        contestants_list_1.pop(contestants_list_1.index(eliminated_one_1))
-        contestant_points_1.update({eliminated_one_1: points_1.get(contestants_eliminated_1)})
+        x += 1
+        starting_number -= 21
+    while contestants_selected <= number_of_contestants:
+        selected_one = random.choice(contestants_list)
+        contestants_list.pop(contestants_list.index(selected_one))
+        contestant_points.update({selected_one: points.get(contestants_selected)})
         # increase contestants_eliminated
-        contestants_eliminated_1 += 1
-        print(contestant_points_1)
+        contestants_selected += 1
+        print(f'{selected_one} was chosen. (contestants selected: {contestants_selected - 1})')
+        print()
+    sorter(contestant_points)
+    eliminator(contestant_points, main_contestants_list)
 
+def wheel_of_misfortune():
+    print()
+    print('Wheel of Misfortune')
+    print('Try not to be selected')
+    points = dict()
+    number_of_contestants = len(main_contestants_list)
+    contestants_list = copy.deepcopy(main_contestants_list)
+    contestants_list_clone = copy.deepcopy(contestants_list)
+    contestants_selected = 1
+    starting_number = 1000
+    x = 1
+    contestant_points = dict()
+    while x <= number_of_contestants:
+        # append points to dictionary
+        points.update({x: starting_number})
+        # increase x by 1
+        x += 1
+        starting_number += 21
+    while contestants_selected <= number_of_contestants:
+        selected_one = random.choice(contestants_list_clone)
+        contestants_list_clone.pop(contestants_list_clone.index(selected_one))
+        contestant_points.update({selected_one: points.get(contestants_selected)})
+        # increase contestants_eliminated
+        contestants_selected += 1
+    sorter(contestant_points)
+    eliminator(contestant_points, contestants_list)
+        
 
+# random number comparison type
 
-
+def drive_down_the_m6():
+    print()
+    print('Drive down the M6')
+    print('Drive the furthest down the M6 before running out of fuel')
 # main body
 print('Welcome to the RV Automator.')
 print('What do you want to do?')
@@ -47,14 +144,36 @@ elif answer == "2":
     print('The first 3 events will have no elimination.')
     print('Following that, the next n events will be triple elimination.')
     print('Then, n-3 events will be double elimination.')
-    print('After that, the rest will be single elimination until there are only 3 cotestants left.')
+    print('After that, the rest will be single elimination until there are only 3 contestants left.')
     print('n is the number of contestants divided by 3.')
 elif answer == "1":
+    elimination = 0
+    current_event = 1
     contestants = input('Please enter contestants names separated by commas.')
-    contestants_list = contestants.split(',')
-    wheel_of_fortune()
-    previous_event = 1
-    # main function
-    # ...
+    main_contestants_list = contestants.split(',')
+    while current_event <= 3:
+        contestants_list = copy.deepcopy(main_contestants_list)
+        elimination = 1
+        wheel_of_fortune()
+        # wheel_of_misfortune()
+        current_event += 1
+    #while current_event <= len(main_contestants_list) / 3:
+    #   elimination = 3
+        # n events with triple elimination
 
+    #    current_event += 1    
+    #while current_event <= len(main_contestants_list) / 3 * 2 - 3:
+    #    elimination = 2
+        # n-3 events with double elimination
+
+    #    current_event += 1
+    #while len(main_contestants_list) != 3:
+    #    elimination = 1
+        # events until 3 contestants
+
+    #    current_event += 1
+    # finale
+    # random events
+else:
+    raise TypeError
 
